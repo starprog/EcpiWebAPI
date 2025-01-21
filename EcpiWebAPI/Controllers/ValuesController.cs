c510e225c4888a1f35765fd4ca3c9dfb491cefa3
Keith Wilson <fiercedev@outlook.com>
2025-01-20 20:30:31 -0500

    Add project files.

---
+++ EcpiWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using EcpiWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EcpiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "value1", "value2", "Bob"
+        };
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return ValuesService.GetValues();
+            //return Summaries;
+            // return new string[] { "value1", "value2", "Bob" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            //Summaries.Append(value);
+            ValuesService.AddValues(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EcpiWebAPI/Services/ValuesService.cs
@@ -0,0 +1,30 @@
+﻿namespace EcpiWebAPI.Services
+{
+    public static class ValuesService
+    {
+        private static readonly List<string> Summaries = new List<string>
+        {
+            "value1", "value2", "Bob"
+        };
+
+        public static string[] GetValues()
+        {
+            // Return a copy to prevent direct modification of the list.
+            lock (Summaries)
+            {
+                return Summaries.ToArray();
+            }
+        }
+
+        public static void AddValues(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                lock (Summaries) // Ensure thread safety.
+                {
+                    Summaries.Add(value);
+                }
+            }
+        }
+    }
+}
