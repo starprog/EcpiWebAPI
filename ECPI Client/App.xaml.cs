18caf211c786b94fcfb333502819b30d177157f8
Keith Wilson <fiercedev@outlook.com>
2025-01-24 20:00:28 -0500

    Added a Client side using Connected Services using swagger.

---
+++ ECPI Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ECPI_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ECPI Client/swaggerClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECPI_Client
+{
+    public partial class swaggerClient
+    {
+        public swaggerClient()
+        {
+            _baseUrl = "http://localhost:5000";
+        }
+    }
+}
